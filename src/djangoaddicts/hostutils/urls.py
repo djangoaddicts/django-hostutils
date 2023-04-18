c0ab78e11a0aac0a7ffb57c86e70214650e5e732
David Slusser <dbslusser@gmail.com>
2023-04-17 19:15:12 -0700

    adding tests

--- src/djangoaddicts/hostutils/urls.py
+++ src/djangoaddicts/hostutils/urls.py
@@ -7,12 +7,12 @@ app_name = "hostutils"
 
 urlpatterns = [
     # host views
+    path("host_cpu/", gui.ShowHostCpu.as_view(), name="host_cpu"),
     path("host_details/", gui.ShowHost.as_view(), name="host_details"),
-    path("host_network/", gui.ShowHostNetwork.as_view(), name="host_network"),
-    path("host_process/", gui.ShowHostProcesses.as_view(), name="host_process"),
     path("host_disk/", gui.ShowHostDisk.as_view(), name="host_disk"),
     path("host_memory/", gui.ShowHostMemory.as_view(), name="host_memory"),
-    path("host_cpu/", gui.ShowHostCpu.as_view(), name="host_cpu"),
+    path("host_network/", gui.ShowHostNetwork.as_view(), name="host_network"),
+    path("host_process/", gui.ShowHostProcesses.as_view(), name="host_process"),
     # ajax views
     path("host_interface_stats/", ajax.GetHostNetworkStats.as_view(), name="host_interface_stats"),
     path("host_process_details/", ajax.GetHostProcessDetails.as_view(), name="host_process_details"),

022e3206ab2820dc52ebb09cdedcf8dbd936f80e
David <36340871+davidslusser@users.noreply.github.com>
2023-04-28 15:37:40 -0700

    removing duplicate templates; updating setup config (#5)

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ from setuptools import setup
 # https://github.com/pypa/setuptools_scm#importing-in-setuppy
 
 def myversion():
-    from setuptools_scm.version import SEMVER_MINOR, guess_next_simple_semver, release_branch_semver_version
-
+    from setuptools_scm.version import SEMVER_MINOR, SEMVER_PATCH, guess_next_simple_semver, release_branch_semver_version
     def my_release_branch_semver_version(version):
+        print("TEST: ", version)
         v = release_branch_semver_version(version)
-        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
-            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
+        print('TEST: ', v)
+        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_PATCH):
+            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_PATCH)
+        print('TEST: ', v)
         return v
 
     return {

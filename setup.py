f3ee98e1e6875142707698b44b052d0695ef36a7
David <36340871+davidslusser@users.noreply.github.com>
2023-04-28 15:56:21 -0700

    Cleanup setup (#6)
    * updating pytest install
    
    * restoring pytest opts
    
    * remove commented out import

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup
 
-# setup(use_scm_version=True)
-
 
 # https://stackoverflow.com/questions/75091265/python-setuptools-scm-get-version-from-git-tags
 # https://github.com/pypa/setuptools_scm#importing-in-setuppy
 
 def myversion():
-    from setuptools_scm.version import SEMVER_MINOR, SEMVER_PATCH, guess_next_simple_semver, release_branch_semver_version
+    from setuptools_scm.version import SEMVER_PATCH, guess_next_simple_semver, release_branch_semver_version
     def my_release_branch_semver_version(version):
-        print("TEST: ", version)
         v = release_branch_semver_version(version)
-        print('TEST: ', v)
         if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_PATCH):
             return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_PATCH)
-        print('TEST: ', v)
         return v
 
     return {

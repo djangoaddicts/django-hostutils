d51277d76daa806daa313f6033aca40edea5f358
David <36340871+davidslusser@users.noreply.github.com>
2023-09-24 16:51:46 -0700

    updating htmx modals (#13)

--- src/djangoaddicts/hostutils/apps.py
+++ src/djangoaddicts/hostutils/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CodegenConfig(AppConfig):
+class HostUtilsConfig(AppConfig):
     name = "djangoaddicts.hostutils"
     verbose_name = "Django Host Utilities"
